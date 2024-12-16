5678b08406bc2b9d670ee49d1f247152ccebe7b7
nedhmn <hermannned@gmail.com>
2024-12-15 21:45:50 -0500

    feat: highchart-scatterplot-imgs

--- dependencies.R
+++ dependencies.R
@@ -3,7 +3,11 @@
 # Description: Project dependencies
 # ---------------------------------------------------------------------------- #
 
+library(data.table)
+library(highcharter)
 library(httpgd)
 library(languageserver)
 library(rmarkdown)
+library(rmarkdown)
 library(testthat)
+library(tidyverse)
